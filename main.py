731dc44d457efa8f36d270d562b72aafb54eec6a
bgujjala <bgujjala@deloitte.com>
2021-09-16 14:36:09 +0530

    merge two list

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+list1 = ["Hi" , "bye "]
+list2 = ["day", "month"]
+resList = [x+y for x in list1 for y in list2]
+print(resList)
